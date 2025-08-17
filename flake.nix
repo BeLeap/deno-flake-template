84ccd4d220f2a7855ff673c346afbc8999120ec4
BeLeap <beleap@beleap.dev>
2025-08-17 15:37:59 +0900

    feat: use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,28 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-    (builtins.foldl'
-      (
-        acc: system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        (
-          acc
-          // {
-            packages.${system}.default = import ./default.nix { inherit pkgs; };
-            devShells.${system}.default = import ./shell.nix { inherit pkgs; };
-          }
-        )
-      )
-      { }
-      [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ]
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = import ./default.nix { inherit pkgs; };
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
     );
 }
