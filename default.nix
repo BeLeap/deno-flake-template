6747a5eeef38e27ed890c450e52968ab9491f67e
Changseo Jang <beleap@beleap.dev>
2025-08-17 23:27:41 +0900

    remove configure phase

--- default.nix
+++ default.nix
@@ -10,12 +10,6 @@ pkgs.stdenv.mkDerivation {
     deno
   ];
 
-  configurePhase = ''
-    export DENO_DIR=$TMP/deno
-    deno install --entrypoint $src/main.ts
-    deno compile https://docs.deno.com/examples/welcome.ts
-  '';
-
   buildPhase = ''
     DENO_DIR=$TMP/deno deno compile -A -o example $src/main.ts
   '';
