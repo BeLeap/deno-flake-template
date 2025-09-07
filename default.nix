b736c9c1d453df472a5d91be730b33fcfb762057
Changseo Jang <beleap@beleap.dev>
2025-09-07 23:09:33 +0900

    feat: add run hook

--- default.nix
+++ default.nix
@@ -11,11 +11,15 @@ pkgs.stdenv.mkDerivation {
   ];
 
   buildPhase = ''
+    runHook preBuild
     DENO_DIR=$TMP/deno deno compile -A -o example $src/main.ts
+    runHook postBuild
   '';
 
   installPhase = ''
+    runHook preInstall
     mkdir -p $out/bin
     install -m755 example $out/bin/example
+    runHook postInstall
   '';
 }
